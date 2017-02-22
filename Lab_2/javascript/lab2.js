436de2f8d96cc7a839fc002eee891e324b8502b5
Alam <2018aalam@local.tjhsst.edu>
2017-02-22 12:00:16 -0500

    added 50 states game

--- Lab_2/javascript/lab2.js
+++ Lab_2/javascript/lab2.js
@@ -1,6 +1,33 @@
+var score = 0;
+var state;
 function getBorderStates()
 {
-    var state = document.getElementById("user_input").value;
-    document.getElementById("state").innerHTML = state;
+    var borderstates =  states.usa[state];
+    var answer = document.getElementById("user_input").value.split(", ");
+    if(array1.sort().compare(array2.sort())) {
+     doSomething();
+} else {
+    doAnotherThing();
+    }
     
 }
+Array.prototype.compare = function(testArr) {
+    if (this.length != testArr.length) return false;
+    for (var i = 0; i < testArr.length; i++) {
+        if (this[i].compare) { //To test values in nested arrays
+            if (!this[i].compare(testArr[i])) return false;
+        }
+        else if (this[i] !== testArr[i]) return false;
+    }
+    return true;
+}
+function getRandomState()
+{
+	state = Object.keys(states.usa)[parseInt(Math.random() * 50)]
+	document.getElementById("state").innerHTML = "Which states border " + state + "?";
+}
+function getScore()
+{
+	return score;
+}
+getRandomState();
